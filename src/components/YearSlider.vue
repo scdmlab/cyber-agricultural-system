4ffdd0054578a553f73dd0be0a15c5821f4beaba
Heyweol <112049812+Heyweol@users.noreply.github.com>
2024-10-18 04:32:36 -0500

    add selector to slider

--- src/components/YearSlider.vue
+++ src/components/YearSlider.vue
@@ -1,24 +1,29 @@
 <template>
-    <div v-if="isVisible" class="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-50 bg-white bg-opacity-80 p-2 rounded shadow-md flex flex-col items-center w-3/4 max-w-3xl">
-      <div class="flex items-center w-full mb-2">
-        <button @click="togglePlay" class="mr-2 p-1 bg-blue-500 text-white rounded">
-          {{ isPlaying ? '⏸' : '▶' }}
-        </button>
-        <input
-          type="range"
-          :min="2001"
-          :max="2023"
-          :value="currentYear"
-          @input="updateYear"
-          class="flex-grow appearance-none bg-gray-200 h-1 rounded-full outline-none slider-thumb"
-        />
-      </div>
-      <div class="text-xs font-bold">Year: {{ currentYear }} &nbsp;&nbsp; {{ currentProperty }}</div>
+  <div v-if="isVisible" class="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-50 bg-white bg-opacity-80 p-2 rounded shadow-md flex flex-col items-center w-3/4 max-w-3xl">
+    <div class="flex items-center w-full mb-2">
+      <button @click="togglePlay" class="mr-2 p-1 bg-blue-500 text-white rounded">
+        {{ isPlaying ? '⏸' : '▶' }}
+      </button>
+      <select v-model="selectedProperty" class="mr-2 p-1 bg-white border border-gray-300 rounded">
+        <option value="yield">Yield</option>
+        <option value="pred">Prediction</option>
+        <option value="error">Error</option>
+      </select>
+      <input
+        type="range"
+        :min="2001"
+        :max="2023"
+        :value="currentYear"
+        @input="updateYear"
+        class="flex-grow appearance-none bg-gray-200 h-1 rounded-full outline-none slider-thumb"
+      />
     </div>
+    <div class="text-xs font-bold">Year: {{ currentYear }} &nbsp;&nbsp; {{ currentProperty }}</div>
+  </div>
 </template>
 
 <script>
-import { ref, computed, onUnmounted } from 'vue'
+import { ref, computed, onUnmounted, watch } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
@@ -29,13 +34,13 @@ export default {
     let intervalId = null
 
     const currentYear = computed(() => store.state.currentYear)
+    const selectedProperty = ref(store.state.currentProperty)
     const propertyMap = {
-        'yield': 'Yield',
-        'pred':'Model Prediction',
-        'error':'Prediction Error',
-        'uncertainty':'Uncertainty'
+      'yield': 'Yield',
+      'pred': 'Model Prediction',
+      'error': 'Prediction Error'
     }
-    const currentProperty = computed(() => propertyMap[store.state.currentProperty])
+    const currentProperty = computed(() => propertyMap[selectedProperty.value])
     const isVisible = computed(() => store.state.yearSliderVisible)
 
     const updateYear = (event) => {
@@ -69,13 +74,18 @@ export default {
       stopPlaying()
     })
 
+    watch(selectedProperty, (newValue) => {
+      store.commit('setProperty', newValue)
+    })
+
     return {
       currentYear,
       currentProperty,
       updateYear,
       isVisible,
       isPlaying,
-      togglePlay
+      togglePlay,
+      selectedProperty
     }
   }
 }
