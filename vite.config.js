5fa3954dae17d854796a2d679c1d3d8e7f509e89
Heyweol <112049812+Heyweol@users.noreply.github.com>
2025-01-16 07:45:50 -0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue()],
+  base: "/cyber-agricultural-system/",
+  plugins: [
+    vue(),
+    vueDevTools({
+      launchEditor: "webstorm",
+    })
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   build: {
     outDir: 'dist',
